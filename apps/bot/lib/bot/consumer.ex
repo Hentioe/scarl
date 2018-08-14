17244d424c7033e4372aea9d6b32d0422b142901
Hentioe <me@bluerain.io>
2018-08-15 03:43:55 +0800

    Add  FlagRouter by message content

--- apps/bot/lib/bot/consumer.ex
+++ apps/bot/lib/bot/consumer.ex
@@ -3,17 +3,41 @@ defmodule Bot.Consumer do
   Bot 消费消息
   """
   use Nostrum.Consumer
-
+  use Bot.{FlagRouter}
   alias Nostrum.Api
 
   def start_link do
     Consumer.start_link(__MODULE__, name: __MODULE__)
   end
 
+  def handle_flag(:records, _args, msg) do
+    Api.create_message(msg.channel_id, "战绩查询")
+  end
+
+  def handle_flag(:help, _args, msg) do
+    Api.create_message(msg.channel_id, "我是一条帮助消息")
+  end
+
+  def handle_onlyat(msg) do
+    handle_flag(:help, [], msg)
+  end
+
+  def handle_unknown_flag(flag, _args, msg) do
+    Api.create_message(msg.channel_id, "无法理解的指令: #{flag}")
+  end
+
+  def handle_flag(_) do
+    nil
+  end
+
+  @scar_binary ".scar"
   def handle_event({:MESSAGE_CREATE, {msg}, _ws_state}) do
     case msg.content do
-      "ping" ->
-        Api.create_message(msg.channel_id, "I copy and pasted this code")
+      <<@scar_binary, 32, data::binary>> ->
+        routing_in_message(data, msg)
+
+      <<@scar_binary>> ->
+        handle_onlyat(msg)
 
       _ ->
         :ignore
---
+++ apps/bot/lib/bot/flag_router.ex
@@ -0,0 +1,48 @@
+defmodule Bot.FlagRouter do
+  @moduledoc """
+  Bot 消息指令路由
+  """
+
+  alias Nostrum.Struct.{Message}
+
+  @type error :: {:error, Nostrum.Error.ApiError.t() | HTTPoison.Error.t()}
+
+  defmacro __using__(_opts) do
+    quote do
+      @behaviour Bot.FlagRouter
+
+      @allow_flag_list [
+        "help",
+        "records"
+      ]
+
+      defp routing_in_message(data, msg) when is_binary(data) do
+        [flag | args] = String.split(data, " ")
+
+        cond do
+          Enum.member?(@allow_flag_list, flag) ->
+            handle_flag(String.to_atom(flag), args, msg)
+
+          flag == "" ->
+            handle_onlyat(msg)
+
+          true ->
+            handle_unknown_flag(flag, args, msg)
+        end
+      end
+
+      def handle_flag(flag, args, _msg) do
+        IO.puts("[默认指令处理函数] Flag: #{Atom.to_string(flag)}, args: #{List.to_string(args)}")
+      end
+
+      defoverridable handle_flag: 3
+    end
+  end
+
+  @callback handle_flag(flag :: Atom.t(), args :: List.t(), msg :: term) ::
+              error | {:ok, Message.t()}
+
+  @callback handle_onlyat(msg :: term) :: error | {:ok, Message.t()}
+  @callback handle_unknown_flag(flag :: String.t(), args :: List.t(), msg :: term) ::
+              error | {:ok, Message.t()}
+end
